b63024ab3a9af5be6c8f40238ff746143a892188
Octree <fouljz@gmail.com>
2018-08-23 16:19:36 +0800

    🎉 Initial commit.

---
+++ Bencode/Bencode.h
@@ -0,0 +1,19 @@
+//
+//  Bencode.h
+//  Bencode
+//
+//  Created by Octree on 2018/8/23.
+//  Copyright © 2018年 Octree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bencode.
+FOUNDATION_EXPORT double BencodeVersionNumber;
+
+//! Project version string for Bencode.
+FOUNDATION_EXPORT const unsigned char BencodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bencode/PublicHeader.h>
+
+
