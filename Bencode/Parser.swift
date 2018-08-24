4d0edeff4f902803f27e1df4a367714f805bf5b5
Octree <fouljz@gmail.com>
2018-08-24 10:18:17 +0800

    🍻 BencodeParser

--- Bencode/Parser.swift
+++ Bencode/Parser.swift
@@ -11,3 +11,131 @@ import ParserCombinator
 import FP
 
 
+
+let zeroAscii = Character("0").asciiValue
+let nineAscii = Character("9").asciiValue
+
+/// 结束符号
+let ending = character {  $0 == "e" }
+
+
+/// 整数的开始标识
+let integerHead = character {  $0 == "i" }
+
+/// list 的开始标识
+let listHead = character {  $0 == "l" }
+
+/// Dictionary 的开始标识
+let dictHead = character {  $0 == "d" }
+
+/// 冒号
+let colon = character {  $0 == ":" }
+
+/// +
+let positiveSign = character {  $0 == "+" }
+
+/// -
+let negitiveSign = character {  $0 == "-" }
+
+/// 0-9
+let digit = character { ch in
+    let ascii = ch.asciiValue
+    return ascii >= zeroAscii && ascii <= nineAscii
+}
+
+
+/// 1-9
+let nzDigit = character { ch in
+    let ascii = ch.asciiValue
+    return ascii > zeroAscii && ascii <= nineAscii
+}
+
+/// NZ_DIGIT DIGIT*;
+let multiInt =  { rt in Int(String([rt.0] + rt.1))! } <^> nzDigit.followed(by: digit.many1)
+
+///  DIGIT
+let singleInt = { x in Int(String(x))! } <^> digit
+/// INT : DIGIT | NZ_DIGIT DIGIT*;
+let int = multiInt <|> singleInt
+
+private func integerTransformer(sign: Character?, num: Int) -> Int {
+    guard let s = sign else {
+        return num
+    }
+    
+    if s == "+" {
+        return num
+    } else {
+        return -num
+    }
+}
+
+let signedInt = integerTransformer <^> (positiveSign <|> negitiveSign).optional.followed(by: int)
+
+func takeNCharacters(n: Int) -> Parser<Substring, String> {
+    
+    return Parser {
+        input in
+        guard input.count >= n else {
+            return .fail(ParserError.eof)
+        }
+        
+        let index = input.index(input.startIndex, offsetBy: n)
+        let string = String(input[..<index])
+        return .done(input.dropFirst(n), string)
+    }
+}
+
+//    string     : INT ':' stringData[$INT.int];
+func string() -> Parser<Substring, BencodeValue> {
+    
+    return { x in BencodeValue.string(x) } <^> ((int <* colon) >>- takeNCharacters)
+}
+
+//    integer    : 'i' sign=('+'|'-')? INT 'e';
+func integer() -> Parser<Substring, BencodeValue> {
+    
+    
+    return { x in BencodeValue.integer(x) } <^> signedInt.between(open: integerHead, close: ending)
+}
+
+func dict() -> Parser<Substring, BencodeValue> {
+    
+    let parseKV = (dictHead >>- {
+        input in
+        return string().followed(by: value()).many
+        }) <* ending
+    
+    return {
+        (lst: [(BencodeValue, BencodeValue)]) in
+        
+        var dict = [String: BencodeValue]()
+        for elt in lst {
+            if case let .string(key) = elt.0 {
+                dict[key] = elt.1
+            }
+        }
+        return BencodeValue.dict(dict)
+        } <^> parseKV
+}
+
+/// list       : 'l' value* 'e';
+func list() -> Parser<Substring, BencodeValue> {
+    
+    let parseLst = (listHead >>- {
+        input in
+        return value().many
+        }) <* ending
+    
+    return { BencodeValue.list($0) } <^> parseLst
+}
+
+/// dictionary : 'd' (string value)* 'e';
+func value() -> Parser<Substring, BencodeValue> {
+    
+    return list() <|> dict() <|> integer() <|> string()
+}
+
+
+/// BencodeParser
+public let BencodeParser = value()
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Bencode
+
+
+func test() {
+    
+    print(BencodeParser.parse("i-123123e"))
+    print(BencodeParser.parse("14:helloworld helloworld"))
+    print(BencodeParser.parse("l7:tolstoyi42ee"))
+    print(BencodeParser.parse("d6:string11:Hello World7:integeri12345e4:dictd3:key36:This is a string within a dictionarye4:listli1ei2ei3ei4e6:stringi5edeee"))
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        test()
     }
 
 
