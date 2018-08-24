956ba7ea4b17f6d6a54283278984a68a295a8460
Octree <fouljz@gmail.com>
2018-08-24 11:24:12 +0800

    🍻 Writing code drunkenly.

--- Bencode/BDecoder.swift
+++ Bencode/BDecoder.swift
@@ -1,12 +1,9 @@
 //
-//  Bencoder.swift
+//  BDecoder.swift
 //  Bencode
 //
-//  Created by Octree on 2018/8/23.
+//  Created by Octree on 2018/8/24.
 //  Copyright © 2018年 Octree. All rights reserved.
 //
 
 import Foundation
-import ParserCombinator
-import FP
-
