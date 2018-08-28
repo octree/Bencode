215174032d8549d14384af6806e89de26497b6d0
Octree <fouljz@gmail.com>
2018-08-28 14:45:42 +0800

    🚧 _BencodingStorage

--- Bencode/Encoder/_BEncodingStorage.swift
+++ Bencode/Encoder/_BEncodingStorage.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct _BEncodingStorage {
+    
+    private(set) var containers: [Any] = []
+    init() { }
+    var count: Int { return containers.count }
+    
+    mutating func pushKeyedContainer() -> NSMutableDictionary {
+        
+        let dictionary = NSMutableDictionary()
+        self.containers.append(dictionary)
+        return dictionary
+    }
+    
+    mutating func pushUnkeyedContainer() -> NSMutableArray {
+        
+        let array = NSMutableArray()
+        self.containers.append(array)
+        return array
+    }
+    
+    mutating func push(container: NSObject) {
+        
+        self.containers.append(container)
+    }
+    
+    mutating func pop() -> Any {
+        
+        precondition(!self.containers.isEmpty, "Empty container stack.")
+        return self.containers.popLast()!
+    }
+}
