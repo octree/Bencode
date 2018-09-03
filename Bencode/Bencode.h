1d97b1e700c30037a5462bc5122b7eaffbad39dc
Octree <fouljz@gmail.com>
2018-09-03 11:13:09 +0800

    🍎 support macOS

--- Bencode/Bencode.h
+++ Bencode/Bencode.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018年 Octree. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Bencode.
 FOUNDATION_EXPORT double BencodeVersionNumber;
